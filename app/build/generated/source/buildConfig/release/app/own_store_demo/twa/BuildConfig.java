9df656540f50dd3a452ecb2f5eb968fce07124e8
Faiyaz Shaikh <www.shtakkar@gmail.com>
2022-01-27 19:16:31 +0530

    feat: init

---
+++ app/build/generated/source/buildConfig/release/app/own_store_demo/twa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.own_store_demo.twa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "app.own_store_demo.twa";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
+}
---
+++ app/src/main/java/app/own_store_demo/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package app.own_store_demo.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
